9be21730f4567a3850ea097351fdab240ce00152
Alexander Guschin <alexander.ragon@gmail.com>
2015-09-15 14:47:24 +0300

    Adding MentionImageTableViewCell public and private API

--- Smashtag/MentionImageTableViewCell.swift
+++ Smashtag/MentionImageTableViewCell.swift
@@ -10,4 +10,30 @@ import UIKit
 
 class MentionImageTableViewCell: UITableViewCell {
     @IBOutlet weak var mentionImage: UIImageView!
+    
+    //MARK: - Public API
+    var imageURL: NSURL? {
+        didSet {
+            fetchImage()
+        }
+    }
+    
+    //MARK: - Private API
+    private func fetchImage() {
+        if let url = imageURL {
+            let qos = Int(QOS_CLASS_USER_INITIATED.value)
+            dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
+                let imageData = NSData(contentsOfURL: url)
+                dispatch_async(dispatch_get_main_queue()) {
+                    if url == self.imageURL {
+                        if imageData != nil {
+                            self.mentionImage.image = UIImage(data: imageData!)
+                        } else {
+                            self.mentionImage = nil
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
