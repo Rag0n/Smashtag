e1fbccf36ed170fb6bf6fe07c210d3e3d27ceeb6
Alexander Guschin <alexander.ragon@gmail.com>
2015-09-21 14:10:15 +0300

    Adding show tweet from history segue

--- Smashtag/HistoryTableViewController.swift
+++ Smashtag/HistoryTableViewController.swift
@@ -37,15 +37,17 @@ class HistoryTableViewController: UITableViewController {
     }
 
 
+     // MARK: - Navigation
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "show tweet from history" {
+            if let ttvc = segue.destinationViewController as? TweetTableViewController {
+                if let selectedIndex = tableView.indexPathForCell(sender as! UITableViewCell) {
+                    ttvc.searchText = history.searchHistory[selectedIndex.row]
+                }
+            }
+        }
     }
-    */
+
 
 }
